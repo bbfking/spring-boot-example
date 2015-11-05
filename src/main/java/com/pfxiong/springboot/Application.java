312756502a38fdde1379b10c493c936cab9cd14f
pfxiong <bbfking@gmail.com>
2015-11-05 17:39:47 +0800

    添加日志

--- src/main/java/com/pfxiong/springboot/Application.java
+++ src/main/java/com/pfxiong/springboot/Application.java
@@ -1,11 +1,24 @@
 package com.pfxiong.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
-	public static void main(String[] args) {
+    private static Logger logger = LogManager.getLogger(Application.class);// 日志
+    @RequestMapping("/")
+    public String home() {
+        logger.info("get home");
+        return "Hello spring boot";
+    }
+
+    public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 }
